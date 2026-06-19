13e9cbadda294011f214317cd5e8e3136a8d08a3
Hoang Nguyen <nguyenviethoang1031@gmail.com>
2026-06-18 22:10:05 -0400

    use axios to fetch data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Header } from '../components/Header.jsx'
 import CheckMarkIcon from '../assets/images/icons/checkmark.png'
 import { products } from '../../starting-code/data/products'
@@ -5,11 +6,9 @@ import './HomePage.css'
 
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      response.json();
-    }).then((data) => {
-      console.log(data);
+      console.log(response.data);
     });
 
   return (
