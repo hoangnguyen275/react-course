8e19d2ea01c4de2aec9c79af710fea21daf2bad0
Hoang Nguyen <nguyenviethoang1031@gmail.com>
2026-06-18 22:07:07 -0400

    fetch data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,6 +5,13 @@ import './HomePage.css'
 
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      response.json();
+    }).then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
