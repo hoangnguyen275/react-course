a956b872bd97e4df37673a44922eeb4fae18f5e7
Hoang Nguyen <nguyenviethoang1031@gmail.com>
2026-06-18 22:40:21 -0400

    lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { Routes, Route } from 'react-router';
+import { useState, useEffect} from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrderPage } from './pages/OrderPage';
@@ -7,10 +9,20 @@ import { Error404Page } from './pages/Error404Page';
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+        console.log(response.data);
+      });
+  });
+
   return (
     <Routes>
-      <Route index element={<HomePage/>} />
-      <Route path="checkout" element={<CheckoutPage/>} />
+      <Route index element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrderPage/>} />
       <Route path="tracking" element={<TrackingPage/>}/>
       <Route path="*" element={<Error404Page/>} />
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,21 +5,15 @@ import CheckMarkIcon from '../assets/images/icons/checkmark.png'
 import './HomePage.css'
 
 
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
   
   useEffect(() => {
     axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
       });
-    
-    axios.get('/api/cart-items')
-      .then((response) => {
-        setCart(response.data);
-        console.log(response.data);
-      });  
+
   }, []);
 
   return (
