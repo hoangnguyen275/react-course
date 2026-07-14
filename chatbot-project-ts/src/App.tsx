91e9ba929ac8d5e65ee42f8b4f4b4a2d28b441b2
Hoang Nguyen <nguyenviethoang1031@gmail.com>
2026-07-14 19:40:00 -0400

    convert some jsx files to tsx files, add  "allowJs": true in tsconfig.app.json

---
+++ chatbot-project-ts/src/App.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import { ChatInput } from './components/ChatInput'
+import ChatMessages from './components/ChatMessages'
+import robotImage from './assets/robot.png'
+import { WelcomeMessage } from './components/WelcomeMessage'
+import { Chatbot } from 'supersimpledev'
+import './app.css'
+
+function App() {
+
+  const [title, setTitle] = useState('Chatbot Project');
+
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages') || '[]'));
+  // const [chatMessage, setChatMessage] = array;
+  // const chatMessages = array[0];
+  // const setChatMessages = array[1];
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      "What's up dawg?": "My dawggggg, how can I help you?"
+    });
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }, [chatMessages])
+
+
+  useEffect(() => {
+    if (chatMessages.length > 0) {
+      setTitle(`${chatMessages.length} Messages`);
+    } else{
+      setTitle('Chatbot Project');
+    }
+  }, [chatMessages])
+
+  return (
+    <>
+      <title>{title}</title>
+
+      <link rel="icon" type="image/svg+xml" href={robotImage} />
+
+      <div className="app-container">
+        <ChatMessages
+          chatMessages={chatMessages}
+        />
+
+        <WelcomeMessage
+          chatMessages={chatMessages}
+        />
+
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ chatbot-project-ts/src/components/ChatMessage.jsx
@@ -0,0 +1,58 @@
+import RobotProfileImage from '../assets/robot.png'
+import UserProfileImage from '../assets/user.png'
+import './ChatMessage.css'
+import dayjs from 'dayjs'
+
+export function ChatMessage({message, sender, timeSent}){
+  //const message = props.message;
+  //cosnt sender = sender.message;
+  //const {sender, message} = props;
+  /*
+    if (sender === 'user'){
+    return (
+      <div>
+        {message}
+        <img width="50" src="user.png"/>
+      </div>
+    );
+  } else {
+      return (
+        <div>
+          <img width="50" src="robot.png"/>
+          {message}
+        </div>
+      );
+    }
+  */
+  
+  return (
+        <div className={
+          sender === 'robot' 
+            ? 'chat-message-robot' 
+            : 'chat-message-user'
+        }>
+          {sender === 'robot' && (
+            <img className="chat-message-profile" 
+              src={RobotProfileImage}
+            />
+          )}
+          <div className="chat-message-text">
+            <div>
+              {message}
+            </div>
+            {timeSent ? 
+            <div className="message-time">
+              {dayjs(timeSent).format('h:mm:ssa')}
+            </div> : 
+            ''
+            }
+          </div>
+          {sender ==='user' && (
+            <img className="chat-message-profile"
+              src={UserProfileImage}
+            />
+          )}
+        </div>
+      );
+  
+}
